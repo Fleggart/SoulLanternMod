e50869c5bd492bb6405bfb2f3f99016639793c5d
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-07 03:19:49 +0800

    Create Reference.java

---
+++ src/main/java/com/example/soullantern/util/Reference.java
@@ -0,0 +1,6 @@
+package com.example.soullantern.util;
+
+public class Reference {
+    public static final String MOD_ID = "soullantern";
+    public static final String MOD_NAME = "Soul Lantern Mod";
+}
