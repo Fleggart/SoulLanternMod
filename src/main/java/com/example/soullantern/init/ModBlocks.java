27ab85c3d415a4c25b0c749128f0fb2e043a0e54
Fleggart <151512956+Fleggart@users.noreply.github.com>
2026-09-07 03:39:55 +0800

    Update ModBlocks.java

--- src/main/java/com/example/soullantern/init/ModBlocks.java
+++ src/main/java/com/example/soullantern/init/ModBlocks.java
@@ -1,58 +1,27 @@
 package com.example.soullantern.init;
 
+import com.example.soullantern.SoulLanternMod;
 import com.example.soullantern.blocks.BlockSoulLantern;
-import com.example.soullantern.blocks.BlockSoulTorch;
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = SoulLanternMod.MOD_ID)
 public class ModBlocks {
-    public static final List<Block> BLOCKS = new ArrayList<>();
-    public static final List<Item> ITEMS = new ArrayList<>();
-
     public static final Block SOUL_LANTERN = new BlockSoulLantern();
-    public static final Block SOUL_TORCH = new BlockSoulTorch();
-
-    public static void init() {
-        // 方块已在静态初始化中注册
-    }
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
-        event.getRegistry().registerAll(BLOCKS.toArray(new Block[0]));
+        event.getRegistry().register(SOUL_LANTERN);
     }
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        // 为方块创建 ItemBlock
-        for (Block block : BLOCKS) {
-            ItemBlock itemBlock = new ItemBlock(block);
-            itemBlock.setRegistryName(block.getRegistryName());
-            ITEMS.add(itemBlock);
-        }
-        event.getRegistry().registerAll(ITEMS.toArray(new Item[0]));
-    }
-
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public static void registerModels(ModelRegistryEvent event) {
-        for (Item item : ITEMS) {
-            ModelLoader.setCustomModelResourceLocation(
-                item, 0,
-                new ModelResourceLocation(item.getRegistryName(), "inventory")
-            );
-        }
+        ItemBlock itemBlock = new ItemBlock(SOUL_LANTERN);
+        itemBlock.setRegistryName(SOUL_LANTERN.getRegistryName());
+        event.getRegistry().register(itemBlock);
     }
 }
